bb73a4f0582cab31ac70753187e877c14172f808
thiagoraquel <thiaguinho2005@yahoo.com.br>
2026-06-16 11:45:44 -0300

    setup inicial

---
+++ backend/src/main/java/br/ufrn/academix/academix_fw/AcademixFwApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.academix.academix_fw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademixFwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademixFwApplication.class, args);
+	}
+
+}
