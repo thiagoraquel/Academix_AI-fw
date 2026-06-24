dac4285b451bc671c868ed8d449f2fcddf96740d
thiagoraquel <thiaguinho2005@yahoo.com.br>
2026-06-24 09:41:27 -0300

    Refactor: conta base agora é um ponto fixo ao invés de um ponto variável

--- backend/src/main/java/br/ufrn/academix/framework/core/auth/FrameworkAccount.java
+++ backend/src/main/java/br/ufrn/academix/framework/core/auth/FrameworkAccount.java
@@ -3,13 +3,15 @@ package br.ufrn.academix.framework.core.auth;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Entity
+@Table(name = "framework_accounts")
 @Getter
 @Setter
-@MappedSuperclass
-public abstract class BaseAccount {
+public class FrameworkAccount {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -21,12 +23,9 @@ public abstract class BaseAccount {
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt = LocalDateTime.now();
-
     @Column(nullable = false)
     private String name;
 
-    // O framework exige que a entidade base informe o identificador de negócio para o Motor de IA
-    public abstract String getBusinessIdentifier(); 
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
 }
